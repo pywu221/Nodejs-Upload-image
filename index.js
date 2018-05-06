be3d2da04fa241b83cf27878a9cb2b088b777e0d
angelina <sheeva.wu@gmail.com>
2018-05-06 11:16:00 +0800

    add upload-image files

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+var fs        = require('fs'),
+    jade      = require('jade'),
+    path      = require('path'),
+    knox      = require('knox'),
+    knoxCopy  = require('knox-copy'),
+    http      = require('http'),
+    Blitline  = require('blitline');
+
+var amazon_url = 'http://s3.amazonaws.com/' + process.env.AWS_S3_BUCKET;
+var knox_params = {
+    key: process.env.AWS_ACCESS_KEY_ID.toString(),
+    secret: process.env.AWS_SECRET_ACCESS_KEY.toString(),
+    bucket: process.env.AWS_S3_BUCKET.toString()
+  }
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
